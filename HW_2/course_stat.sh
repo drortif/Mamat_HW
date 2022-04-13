5f643254d7811f742bcbe0d47d2cbbabcca1464c
drortif <drort@campus.technion.ac.il>
2022-04-13 20:13:23 +0300

    updating

--- HW_2/course_stat.sh
+++ HW_2/course_stat.sh
@@ -19,29 +19,24 @@ fi
 
 mkdir $1_stat
 
-#prints the second column to a temp txt file
-awk '{print $2}' $1.txt > temp_grades.txt
-
-#takes all the rows accept the first one (cuz of the word "grades")
-sed 1,1d temp_grades.txt > grades.txt
-
-rm temp_grades.txt
-
-mv grades.txt $1_stat
+#prints the second column to a temp txt file without the word "grades"
+awk '{print $2}' $1.txt | tail +2 > grades.txt
 
 #cwd is the one we created
+mv grades.txt $1_stat
+
+#move the c file
+cp main.c $1_stat
+
 cd $1_stat
 
 #finds out the num of studets
 num_of_students=$(wc -l < grades.txt)
 
-#put it in a file cuz we didnt find out how to send a int veriable
-num_of_students > num_of_students.txt
-
 #compiles the c prog with the grades and nym of students as args
-gcc -g -Wall calc_statistics.c -o prog.exe
+gcc -g -Wall main.c -o prog.exe
 
-./prog.exe grades.txt num_of_students.txt
+./prog.exe grades.txt $num_of_students
 
 #prints the result created by the c prog
 cat course_statistics.txt
